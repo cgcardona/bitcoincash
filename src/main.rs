05e5771e0e0725d1c769fbee6bbd10f8e05b5db9
Gabriel Cardona <cgcardona@gmail.com>
2019-10-25 11:30:27 -0700

    Refactoring to clean up code.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 extern crate bitcoin;
 extern crate secp256k1;
 
-use bitcoin::network::constants::Network;
-use bitcoin::util::address::Address;
-use bitcoin::util::key;
-use secp256k1::rand::thread_rng;
-use secp256k1::Secp256k1;
+use bitcoin::{
+    network::constants::Network,
+    util::{address::Address, key, key::PublicKey},
+};
+use secp256k1::{rand::thread_rng, Secp256k1};
 
 fn main() {
-    let s = Secp256k1::new();
-    let public_key = key::PublicKey {
+    let s: Secp256k1<_> = Secp256k1::new();
+    let public_key: PublicKey = key::PublicKey {
         compressed: true,
         key: s.generate_keypair(&mut thread_rng()).1,
     };
     // Generate pay-to-pubkey-hash address
-    let address = Address::p2pkh(&public_key, Network::Bitcoin);
+    let address: Address = Address::p2pkh(&public_key, Network::Bitcoin);
     println!("Hello, {}!", address);
 }
