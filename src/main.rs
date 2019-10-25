a3fedfdbea1cea18c9c261cdec4ddcb1e11fba94
Gabriel Cardona <cgcardona@gmail.com>
2019-10-25 11:23:34 -0700

    Generate new keypair and p2pkh legacy addr.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate bitcoin;
+extern crate secp256k1;
+
+use bitcoin::network::constants::Network;
+use bitcoin::util::address::Address;
+use bitcoin::util::key;
+use secp256k1::rand::thread_rng;
+use secp256k1::Secp256k1;
+
 fn main() {
-    println!("Hello, world!");
+    let s = Secp256k1::new();
+    let public_key = key::PublicKey {
+        compressed: true,
+        key: s.generate_keypair(&mut thread_rng()).1,
+    };
+    // Generate pay-to-pubkey-hash address
+    let address = Address::p2pkh(&public_key, Network::Bitcoin);
+    println!("Hello, {}!", address);
 }
