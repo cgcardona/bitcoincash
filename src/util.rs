f6b345bd0add1878136fe85b5117555657d130bc
Gabriel Cardona <cgcardona@gmail.com>
2019-10-25 21:18:35 -0700

    Control struct and impl.

--- src/util.rs
+++ src/util.rs
@@ -1,12 +1,14 @@
 pub use self::address::Address;
 pub use self::block::Block;
 pub use self::blockchain::Blockchain;
+pub use self::control::Control;
 pub use self::crypto::Crypto;
 pub use self::mnemonic::Mnemonic;
 pub use self::slp::SLP;
 mod address;
 mod block;
 mod blockchain;
+mod control;
 mod crypto;
 mod mnemonic;
 mod slp;
---
+++ src/util/control.rs
@@ -0,0 +1,66 @@
+#![allow(non_snake_case)]
+
+use core::result::Result;
+use reqwest::Error;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug)]
+pub struct Control {}
+
+const BASE_URL: &str = "https://rest.bitcoin.com/v2/control/";
+
+#[derive(Deserialize, Debug)]
+pub struct ControlInfo {
+    version: u32,
+    protocolversion: u32,
+    blocks: u32,
+    timeoffset: i32,
+    connections: u32,
+    proxy: String,
+    difficulty: f32,
+    testnet: bool,
+    paytxfee: u32,
+    relayfee: f32,
+    errors: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct NetworkInfo {
+    version: u32,
+    subversion: String,
+    protocolversion: u32,
+    localservices: String,
+    localrelay: bool,
+    timeoffset: i32,
+    networkactive: bool,
+    connections: u32,
+    networks: Vec<Network>,
+    relayfee: f32,
+    excessutxocharge: u32,
+    warnings: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Network {
+    name: String,
+    limited: bool,
+    reachable: bool,
+    proxy: String,
+    proxy_randomize_credentials: bool,
+}
+
+impl Control {
+    pub fn get_info() -> Result<ControlInfo, Error> {
+        let url: String = format!("{}getInfo", BASE_URL);
+        let s_slice: &str = &url[..];
+        let info: ControlInfo = reqwest::get(s_slice)?.json()?;
+        Ok(info)
+    }
+
+    pub fn get_network_info() -> Result<NetworkInfo, Error> {
+        let url: String = format!("{}getNetworkInfo", BASE_URL);
+        let s_slice: &str = &url[..];
+        let info: NetworkInfo = reqwest::get(s_slice)?.json()?;
+        Ok(info)
+    }
+}
